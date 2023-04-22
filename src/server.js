4403b727d1cfd1030c65d8b6e5f9d38c358a7dc7
Fernando Cavalcanti <comododk@gmail.com>
2023-04-22 19:04:48 -0300

    feat: create statefull struct to example

--- src/server.js
+++ src/server.js
@@ -1,8 +1,57 @@
 // CommonJS module syntax (require); ES6 module syntax (import) is not supported in Node.js
 import http from 'http';
 
+/*
+  request: all the information about the request made by the user
+  - create user
+  - list user
+  - edit user
+  - delete user
+
+  - HTTP
+    - Methods: GET, POST, PUT, PATCH, DELETE
+    - URL
+
+  GET => get information
+  POST => create information
+  PUT => update information (multiple fields for example)
+  PATCH => update specific information in the backend
+  DELETE => delete information
+
+  response: all the information about the response that will be sent back to the user
+
+  Application Stateful and Stateless
+    - Stateful: the server remembers the user with application save in memory.
+    - Stateless: the server does not remember the user, the user must send the information again(like from a database).
+
+  Headers
+    - Content-Type: application/json
+*/
+
+const users = []; // Stateful
+
 const server = http.createServer((req, res) => {
-  console.log('Escutando na porta 3333');
+  const { method, url } = req;
+
+  if (method === 'GET' && url === '/users') {
+
+    // Early return
+    return res
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users));
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+    });
+
+    return res.end('Create user')
+  }
+
+  console.log(method, url);
   return res.end('Hello World! Gooooooolll');
 });
 
